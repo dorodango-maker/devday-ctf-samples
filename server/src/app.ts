5c6127b92744619fbd64998e8a26affe29572ff2
dorodango-maker <tippy.gfop@gmail.com>
2024-04-17 17:43:20 +0900

    feat: corsの追加

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,41 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express, { Express, Request, Response } from "express";
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from "cors";
+import indexRouter from './routes/index';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app: Express = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join('views'));
+// ビューエンジンの設定
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// ミドルウェアの設定
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join('public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// ルーティングの設定
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req: any, res: any, next: any) {
+// 404エラーハンドリング
+app.use((req, res, next) => {
   next(createError(404));
-});
-
-// error handler
-app.use(function(err: any, req: any, res: any, next: any) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
